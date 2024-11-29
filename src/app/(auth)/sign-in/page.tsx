8b09531544f2f4ad2b505b3d71c4d2fab204e804
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-11-29 23:56:48 +0530

    toasts

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,17 @@
 'use client'
-import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { signIn } from 'next-auth/react';
+import { useState } from 'react';
+import {useDebounceValue} from 'usehooks-ts'
+const page=()=>{
+  const [username,setUsername]=useState('')
+  const [usefrnameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
-function page() {
-  return (
-    <div>page</div>
-  )
+  const debouncedUsername=useDebounceValue(username,300)
 }
 
 export default page
